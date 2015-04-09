26aee2d14dfb7c91b1292d14473966d20d0d6e27
David Ferrandez Molla <dafemoelche@gmail.com>
2015-04-09 07:05:41 +0200

    Implementacion de idiomas
    - Creacion de clase serializada para los texto de los menus
    - Traduccion al ingles de los textos en castellano.

---
+++ Assets/scripts/Clases/Clases Base/TextosMenus.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using Oscuridad.Enumeraciones;
+
+namespace Oscuridad.Clases
+{
+	/// <summary>
+	/// Clase base para la Traduccion de los textos de los menus
+	/// </summary>
+	[System.Serializable]
+	public class TextosMenus
+	{
+		public string btComenzar;
+		public string btContinuar;
+		public string btOpciones;
+		public string btSalir;
+		public string btPartidaNueva;
+		public string toggleSonido;
+		public string toggleMusica;
+		public string btIdioma;
+		
+		/// <summary>
+		/// Constructor de la clase <see cref="TextosMenus"/>.
+		/// </summary>
+		public TextosMenus()
+		{
+		}
+	}
+}
