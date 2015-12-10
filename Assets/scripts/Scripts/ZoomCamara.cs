1c9647641a7809c1dfa74d167657017236543bc9
David Ferrandez Molla <dafemoelche@gmail.com>
2015-12-10 06:55:49 +0100

    Touch
    - Cambio de evento del mouseclick a mousetouch para android.

--- Assets/scripts/Scripts/ZoomCamara.cs
+++ Assets/scripts/Scripts/ZoomCamara.cs
@@ -10,8 +10,9 @@ public class ZoomCamara : MonoBehaviour
 	[HideInInspector]
 	public Quaternion rotacionInicial;
 	public GameObject escenaVistaCamara;
-
+	
 	private RaycastHit hit;
+	private Camera cameraRef;
 
 	void OnEnable()
 	{
@@ -27,10 +28,65 @@ public class ZoomCamara : MonoBehaviour
 	{
 		posicionInicial = transform.position;
 		rotacionInicial = transform.rotation;
+
+		cameraRef = this.GetComponent<Camera> ();
 	}
 
 	void Update()
 	{
+		#if UNITY_ANDROID
+		if (GameCenter.InstanceRef.controladoraJugador.EstadoJugador.Equals(EstadosJugador.enEspera))
+		{
+				if (Input.touchCount > 0) 
+				{
+						if (Input.GetTouch (0).phase == TouchPhase.Began) 
+						{
+								if (Physics.Raycast (cameraRef.ScreenPointToRay (Input.GetTouch (0).position), out hit, Mathf.Infinity)) 
+								{
+										switch (hit.collider.gameObject.layer) 
+										{
+												//Objeto Interactuable
+												case 8: 
+												{
+														if (GameCenter.InstanceRef.controladoraJuego.camaraActiva.ExisteInteractuable (hit.collider.tag.ToString ())) {
+																GameCenter.InstanceRef.controladoraJuego.interactuablePulsado = GameCenter.InstanceRef.controladoraJuego.escenaActual.Buscar_Interactuable (hit.collider.tag.ToString ());
+																GameCenter.InstanceRef.controladoraJuego.interactuablePulsadoOpciones = hit.collider.gameObject.GetComponent<ObjetoInteractuableConZoom> ();
+																GameCenter.InstanceRef.controladoraJuego.interactuablePulsadoGameObject = hit.collider.gameObject;
+																GameCenter.InstanceRef.controladoraSonidos.Lanzar_Fx (GameCenter.InstanceRef.controladoraSonidos.sonidoAcertarPulsar);
+																GameCenter.InstanceRef.controladoraJugador.EstadoJugador = EstadosJugador.enZoomIn;
+														}
+														break;
+												}
+
+												//Objeto Interactuable Sin Zoom
+												case 9:
+												{
+														GameCenter.InstanceRef.controladoraJuego.EjecutarAccion (hit.collider.gameObject);
+														break;
+												}
+
+												//UI
+												case 5:
+												{
+														break;
+												}
+
+												//Objeto no Interactuable
+												default: 
+												{
+														GameCenter.InstanceRef.controladoraSonidos.Lanzar_Fx (GameCenter.InstanceRef.controladoraSonidos.sonidoFalloPulsar);
+														GameCenter.InstanceRef.controladoraJuego.interactuablePulsado = null;
+
+														break;
+												}
+										}
+								}
+						}
+				}
+		}
+		#endif
+
+		#if UNITY_EDITOR_WIN
 		if (GameCenter.InstanceRef.controladoraJugador.EstadoJugador.Equals(EstadosJugador.enEspera))
 		{
 			if (Input.GetMouseButtonDown(0))
@@ -82,6 +138,7 @@ public class ZoomCamara : MonoBehaviour
 				}
 			}
 		}
+		#endif
 	}
 
 	private void DesactivarHijos(GameObject g) 
